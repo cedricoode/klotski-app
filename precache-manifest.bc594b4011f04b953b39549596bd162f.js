2ce20ef57de83571ae277c42ee24fa009af7ae96
cedricoode <illusion.ever@gmail.com>
2019-01-10 02:37:13 +0100

    Updates

---
+++ precache-manifest.bc594b4011f04b953b39549596bd162f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "3c6b76367068b2953604",
+    "url": "/website/static/js/main.3c6b7636.chunk.js"
+  },
+  {
+    "revision": "1cfc9b129853a2fffa1e",
+    "url": "/website/static/js/1.1cfc9b12.chunk.js"
+  },
+  {
+    "revision": "3c6b76367068b2953604",
+    "url": "/website/static/css/main.8c0b13ea.chunk.css"
+  },
+  {
+    "revision": "1cfc9b129853a2fffa1e",
+    "url": "/website/static/css/1.5879ca45.chunk.css"
+  },
+  {
+    "revision": "148c19af51d988bc9cb0b96d56555a96",
+    "url": "/website/index.html"
+  }
+];
